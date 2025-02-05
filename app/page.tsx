44e017c702d1b2221bb9a804dd27310820927082
suron <suron.maharjan@gmail.com>
2025-02-05 10:07:21 +0545

    fix(page): changed name

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ export default function Home() {
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
       <main className="flex flex-col gap-8 items-center justify-center sm:items-start">
         <h1 className="">
-          Hello Worlsd, <span className="font-montserrat">World</span>
+          Hello, <span className="font-montserrat">World</span>
         </h1>
+
         <Image
           alt="random image"
           src={"https://picsum.photos/200"}
