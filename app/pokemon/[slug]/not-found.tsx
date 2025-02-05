3b08e89f47dac65bdcfe39c4a568499c262df0e9
suron <suron.maharjan@gmail.com>
2025-02-05 13:44:29 +0545

    style(not-found): text changes

--- app/pokemon/[slug]/not-found.tsx
+++ app/pokemon/[slug]/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="min-h-[90dvh] flex flex-col gap-2 items-center justify-center">
-      404 - Page Not Found
+      404 - Pokemon Not Found
       <Link href={"/"} className="px-4 py-2 bg-blue-600 rounded-md">
         Go back
       </Link>
